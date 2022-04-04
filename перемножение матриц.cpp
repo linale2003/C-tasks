cbffc8945c7b7d71d6860b5b4a1c3e70933340a6
Li Nikita <nikita.li2003@mail.ru>
2022-04-04 10:22:43 +1000

    peremnojenie matric

---
+++ перемножение матриц.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	const int N = 3;
+	int i, j, k;
+	double A[N][N];
+	double B[N][N];
+	double C[N][N];
+	srand(time(NULL));
+	cout << "Matrix A:\n";
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			A[i][j] = rand() % 20;
+	cout << "Matrix B:\n";
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			B[i][j] = rand() % 20;
+	cout << "Matrix C=AB:\n";
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			C[i][j] = 0;
+			for (k = 0; k < N; k++)
+				C[i][j] += A[i][k] * B[k][j];
+			cout << C[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
